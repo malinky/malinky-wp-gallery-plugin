211891d70f8ea70f4a2a14e76396433a055391d5
malinky <craig@malinkymedia.com>
2015-04-01 09:16:28 +0100

    Add HTML in openPhotoSwipe function
    This is instead of add to the body on very page. It is just added when
    required.

--- js/main.js
+++ js/main.js
@@ -162,6 +162,11 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
     var openPhotoSwipe = function(index, galleryElement, disableAnimation)
     {
 
+        /*
+         * Append HTML to the body.
+         */
+        jQuery('body').append(galleryHtml);
+        
         var pswpElement = document.querySelectorAll('.pswp')[0],
             gallery,
             options,
@@ -272,8 +277,8 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
         /*
          * Debug responsive.
          */
-        console.log(window.devicePixelRatio);
-        console.log('realViewportWidth ' + gallery.viewportSize.x * Math.min(window.devicePixelRatio, 2.5));
+        //console.log(window.devicePixelRatio);
+        //console.log('realViewportWidth ' + gallery.viewportSize.x * Math.min(window.devicePixelRatio, 2.5));
 
     };
 
@@ -287,6 +292,12 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
         galleryElements[i].setAttribute('data-pswp-uid', i+1);
         galleryElements[i].onclick = onThumbnailsClick;
     }
+
+
+    /*
+     * The block of HTML to add in openPhotoSwipe function.
+     */
+     var galleryHtml = '<div class="pswp" tabindex="-1" role="dialog" aria-hidden="true"><div class="pswp__bg"></div><div class="pswp__scroll-wrap"><div class="pswp__container"><div class="pswp__item"></div><div class="pswp__item"></div><div class="pswp__item"></div></div><div class="pswp__ui pswp__ui--hidden"><div class="pswp__top-bar"><div class="pswp__counter"></div><button class="pswp__button pswp__button--close" title="Close (Esc)"></button><button class="pswp__button pswp__button--share" title="Share"></button><button class="pswp__button pswp__button--fs" title="Toggle fullscreen"></button><button class="pswp__button pswp__button--zoom" title="Zoom in/out"></button><div class="pswp__preloader"><div class="pswp__preloader__icn"><div class="pswp__preloader__cut"><div class="pswp__preloader__donut"></div></div></div></div></div><div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap"><div class="pswp__share-tooltip"></div> </div><button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)"></button><button class="pswp__button pswp__button--arrow--right" title="Next (arrow right)"></button><div class="pswp__caption"><div class="pswp__caption__center"></div></div></div></div></div>';
 
 };
 
