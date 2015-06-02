0e8e455393ad8e1b8bb2995da12e9d0a2411748d
Craig Ramsay <craig@malinkymedia.com>
2015-06-02 12:57:56 +0100

    Update global name $malinky_slider_count

--- malinky-gallery-slider-shortcodes.php
+++ malinky-gallery-slider-shortcodes.php
@@ -19,7 +19,7 @@ add_shortcode( 'malinky-gallery', 'malinky_gallery' );
 function malinky_gallery( $atts )
 {
 
-	global $slider_count;
+	global $malinky_slider_count;
 
 	$atts = shortcode_atts(
 		array(
@@ -70,7 +70,7 @@ function malinky_gallery( $atts )
 			<div class="malinky-gallery-slider-loading"></div>
 			<div class="malinky-gallery-slider-wrapper" itemscope itemtype="http://schema.org/ImageGallery">
 				<meta itemprop="about" content="<?php echo esc_attr( get_the_title() ); ?> Photos" />
-				<ul id="malinky-gallery-slider-<?php echo $slider_count; ?>" class="malinky-gallery-slider">
+				<ul id="malinky-gallery-slider-<?php echo $malinky_slider_count; ?>" class="malinky-gallery-slider">
 		<?php }
 
 				if ( malinky_is_tablet_computer() ) {
@@ -138,6 +138,6 @@ function malinky_gallery( $atts )
 
     }
 
-    $slider_count++;
+    $malinky_slider_count++;
 
 }
